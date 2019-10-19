b25df1392b6bbbc93f4b5b3ba33e3b13d41c6b01
Brendon Fatland <b.fatland@gmail.com>
2019-10-18 21:30:01 -0500

    Refactor the API to use Async and await to deal with promises.

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,15 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID d0c13b615776ea74b0e9e93138ea2a2f5320e0dd8f3079cd72f2049cb4b6e034"
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID d0c13b615776ea74b0e9e93138ea2a2f5320e0dd8f3079cd72f2049cb4b6e034"
+      }
+    });
+    console.log(response.data.results);
   }
 
   render() {
